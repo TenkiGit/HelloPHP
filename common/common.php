5f9d1ba325b545dddc98bf52dc36d77b01381d7e
Tenki <tenki1231@gmail.com>
2018-08-30 11:34:52 +0900

    fix_common

--- common/common.php
+++ common/common.php
@@ -5,10 +5,10 @@ define( "DB_SLAVE" , 1 );
 
 $GLOBALS["SiteConf"] = parse_ini_file ( $_SERVER['DOCUMENT_ROOT'] . "/config/site.conf" ) ;
 $GLOBALS["SiteConf"]["DIR"]["TEMPLATE"]      = $_SERVER['DOCUMENT_ROOT'] . "/" . $GLOBALS["SiteConf"]["DIR"]["TEMPLATE"] ;
-$GLOBALS["SiteConf"]["DIR"]["DOCUMENTROOT"]  = $_SERVER['DOCUMENT_ROOT'] . "/../" . $GLOBALS["SiteConf"]["DIR"]["DOCUMENTROOT"] ;
-$GLOBALS["SiteConf"]["DIR"]["CONFIG"]        = $_SERVER['DOCUMENT_ROOT'] . "/../" . $GLOBALS["SiteConf"]["DIR"]["CONFIG"] ;
-$GLOBALS["SiteConf"]["DIR"]["LIBLARY"]       = $_SERVER['DOCUMENT_ROOT'] . "/../" . $GLOBALS["SiteConf"]["DIR"]["LIBLARY"] ;
-$GLOBALS["SiteConf"]["DIR"]["LOG"]           = $_SERVER['DOCUMENT_ROOT'] . "/../" . $GLOBALS["SiteConf"]["DIR"]["LOG"] ;
+$GLOBALS["SiteConf"]["DIR"]["DOCUMENTROOT"]  = $_SERVER['DOCUMENT_ROOT'] . "/" . $GLOBALS["SiteConf"]["DIR"]["DOCUMENTROOT"] ;
+$GLOBALS["SiteConf"]["DIR"]["CONFIG"]        = $_SERVER['DOCUMENT_ROOT'] . "/" . $GLOBALS["SiteConf"]["DIR"]["CONFIG"] ;
+$GLOBALS["SiteConf"]["DIR"]["LIBLARY"]       = $_SERVER['DOCUMENT_ROOT'] . "/" . $GLOBALS["SiteConf"]["DIR"]["LIBLARY"] ;
+$GLOBALS["SiteConf"]["DIR"]["LOG"]           = $_SERVER['DOCUMENT_ROOT'] . "/" . $GLOBALS["SiteConf"]["DIR"]["LOG"] ;
 
 include_once ( $GLOBALS["SiteConf"]["DIR"]["LIBLARY"] . "/database_control.php"  );       // DB系共通
 
