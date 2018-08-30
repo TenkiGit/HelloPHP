7ef017203ecfde9515018eab14f32933e6be5277
Tenki <tenki1231@gmail.com>
2018-08-30 14:55:02 +0900

    fix_little

--- login.php
+++ login.php
@@ -19,6 +19,7 @@ function main(){
         $email ='';
         $user_id ='';
         $name = '';
+        $user_data = 0;
 
         // $user_data = user_data::get_userdata();//全ユーザーデータの取得
         // <!-- ログインようの関数を作る -->
@@ -26,19 +27,20 @@ function main(){
         $user_data = common::get_olluser($_POST['email'],$_POST['password']);
         var_dump($user_data);//確認用
 
-        //全部参照しサーチ
-        foreach($user_data as $data){
-            if($data['email'] == $_POST['email'] && $data['password'] == $_POST['password'])
-            {
-                $flag = TRUE;
-                $user_id = $data['ID'];
-                $name = $data['user_name'];
-                $email = $data['email'];
-            }
-        }
+        // //全部参照しサーチ
+        // foreach($user_data as $data){
+        //     if($data['email'] == $_POST['email'] && $data['password'] == $_POST['password'])
+        //     {
+        //         $flag = TRUE;
+        //         $user_id = $data['ID'];
+        //         $name = $data['user_name'];
+        //         $email = $data['email'];
+        //     }
+        // }
         
         //Trueの場合
-        if($flag==TRUE){
+        // if($flag==TRUE){
+        if($userdata ==1 ){
             // echo '<h3>ログイン成功</h3>';
             var_dump($flag); 
             setcookie('ID',$user_id,time()+(10));//IDと変数(data)と有効時間
