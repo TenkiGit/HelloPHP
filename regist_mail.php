74205855847103e68ee6dd0e77a60659e0f8ec35
Tenki <tenki1231@gmail.com>
2018-08-31 11:37:32 +0900

    fix_little

---
+++ regist_mail.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once("./common/user_data.php");
+include_once("./common/common.php");
+// include_once("./login.php");
+
+try{
+	main();
+}catch(Exception $e){
+	echo $e;
+}
+
+
+function main(){
+	//テンプレートを指定
+	$template = './template/index.html';
+	$params["user_data"] = [];
+	// $params['ID'];
+
+	
+
+	$contents = common::html_output($template,$params);
+
+	
+
+	//出力
+	echo $contents;
+	
+}
